40c089c8a22c634a006f88ee51b8307b722176ae
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-05-29 07:41:27 -0300

    deleting old non working files

---
+++ nets.cu
@@ -0,0 +1,6 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <iostream>
+#include <stdio.h>
+#include <sdint.h>
+
--- unary_arithmetics.h
+++
@@ -1,5 +0,0 @@
-#include "cell.h"
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
